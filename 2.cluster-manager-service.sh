1512caddf778d91d878017ddc2dde4c8a39b87cf
juhyunkil <wngus0331@gmail.com>
2023-01-06 15:16:46 +0900

    commit 230106 kjh

--- 2.cluster-manager-service.sh
+++ 2.cluster-manager-service.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-arg=$1 # create/c or delete/d
+#$1 create/c or delete/d
 
-if [ "$1" == "delete" ] || [ "$1" == "d" ]; then   
+if [ "$1" == "delete" ] || [ "$1" == "d" ]; then  
     echo kubectl delete -f deployments/cluster-manager-service.yaml
     kubectl delete -f deployments/cluster-manager-service.yaml
 else
--- c.deployment-copy.sh
+++ c.deployment-copy.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
-
-dest_path="/root/workspace/jhk/cluster-manager/deployments"
 password="ketilinux"
 ip="10.0.5.24"
 
-scr_deployment_name=$1
+#$1 deployment/d or " "
 
-cd deployments/
-echo scp $scr_deployment_name root@$ip:$dest_path copying...
-sshpass -p $password scp $scr_deployment_name root@$ip:$dest_path
+if [ "$1" == "deployment" ] || [ "$1" == "d" ]; then  
+    dest_path="/root/workspace/jhk/cluster-manager"
+    echo scp -r deployments root@$ip:$dest_path copying...
+    sshpass -p $password scp -r deployments root@$ip:$dest_path
+else
+    dest_path="/root/workspace/jhk/cluster-manager/deployments"
+    echo scp ./deployments/keti-cluster-manager.yaml root@$ip:$dest_path copying...
+    sshpass -p $password scp ./deployments/keti-cluster-manager.yaml root@$ip:$dest_path
+fi
